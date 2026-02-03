0b1a78b93822b915c7c0cc83ccc2e70e97125108
TJ <tjshotta24@gmail.com>
2026-02-03 10:12:56 +0100

    opgave 1

---
+++ jsjs.js
@@ -0,0 +1,20 @@
+// opgave 1 
+
+let person = {
+    firstname: "hans",
+    lastname: "hansen",
+    age: 92,
+    hometown: "Roskilde",
+    pet: "abe",
+    petnavn: "monkeydonkey"
+    
+}
+
+console.log(person.firstname);
+console.log(person.age);
+
+
+console.log(person["lastname"]);
+console.log(person["petnavn"]);
+
+// opgave 2 
