eba1022db32da96ef9c48a708323fdb8d8d76b3f
TJ <tjshotta24@gmail.com>
2026-02-03 11:02:25 +0100

    opgave 2

--- jsjs.js
+++ jsjs.js
@@ -7,7 +7,7 @@ let person = {
     hometown: "Roskilde",
     pet: "abe",
     petnavn: "monkeydonkey"
-    
+
 }
 
 console.log(person.firstname);
@@ -18,3 +18,23 @@ console.log(person["lastname"]);
 console.log(person["petnavn"]);
 
 // opgave 2 
+
+let person2 = {
+    firstname: "hans",
+    lastname: "hansen",
+    udannelse: true,
+    age: 92,
+    hometown: "Roskilde",
+    hobbies: ["fodbold", "svømning", "boksning"],
+    pet: {
+        type: "abe",
+        navn: "monkeydonkey"
+    },
+
+ 
+}
+
+console.log(person2.pet.type);
+console.log(person2.pet.navn);   
+
+
